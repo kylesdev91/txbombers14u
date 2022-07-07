33afdf0c7cc1de2db43e5c7891dd9550f9cdd736
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-07 16:40:53 -0500

    Part 2 added more forms

--- src/component/mailer.js
+++ src/component/mailer.js
@@ -21,7 +21,9 @@ const Mailer = () => {
       className='container border'
       style={{
         marginTop: '50px',
+        marginBottom: '50%',
         width: '50%',
+        height: '50%',
         backgroundImage: `url('https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/291846095_483133866951235_7392436863046212683_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=-K1LGFAjJw0AX-sAOcw&_nc_ht=scontent-dfw5-2.xx&oh=00_AT_pxrUanTtIwcQJSFbMJE0_4wmbMMxkLBlYTQFENc6-Gg&oe=62CC3535')`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
@@ -36,14 +38,42 @@ const Mailer = () => {
         <label style={{ color: 'white' }}>Parent Name</label>
         <input type='text' name='parentName' className='form-control' />
 
+        <label style={{ color: 'white' }}>Parent Number</label>
+        <input type='number' name='parentPhone' className='form-control' />
+
         <label style={{ color: 'white' }}>Parent Email</label>
-        <input type='email' name='user_email' className='form-control' />
+        <input type='email' name='parentEmail' className='form-control' />
 
         <label style={{ color: 'white' }}>Parent Address</label>
-        <input type='email' name='user_address' className='form-control' />
+        <input type='text' name='parentAddress' className='form-control' />
 
-        <label style={{ color: 'white' }}>Message</label>
-        <textarea name='message' rows='4' className='form-control' />
+        <label style={{ color: 'white' }}>Player Name</label>
+        <input type='text' name='playerName' className='form-control' />
+
+        <label style={{ color: 'white' }}>Player Current Team</label>
+        <input type='text' name='playerCurrentTeam' className='form-control' />
+
+        <label style={{ color: 'white' }}>Player Date Of Birth</label>
+        <input type='text' name='playerDOB' className='form-control' />
+
+        <label style={{ color: 'white' }}>Player Primary Position</label>
+        <input
+          type='text'
+          name='playerPrimaryPosition'
+          className='form-control'
+        />
+
+        <label style={{ color: 'white' }}>Player Secondary Position</label>
+        <input
+          type='text'
+          name='playerSecondaryPosition'
+          className='form-control'
+        />
+
+        <label style={{ color: 'white' }}>
+          Notable Accomplishments / Recognitions
+        </label>
+        <textarea name='message' rows='2' className='form-control' />
         <input
           type='submit'
           value='Send'
